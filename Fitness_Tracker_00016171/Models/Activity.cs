34bbb63b5f22a776cf76bbeef333a3a168baca59
00016171 <00016171wiut@gmail.com>
2024-12-04 01:11:13 -0700

    Add Models

---
+++ Fitness_Tracker_00016171/Models/Activity.cs
@@ -0,0 +1,14 @@
+﻿namespace Fitness_Tracker_00016171.Models
+{
+    public class Activity
+    {
+        public int Id { get; set; } 
+        public int UserId { get; set; } 
+        public DateTime ActivityDate { get; set; }
+        public string ActivityType { get; set; } 
+        public double Duration { get; set; } 
+        public double Distance { get; set; } 
+
+        public User User { get; set; }
+    }
+}
---
+++ Fitness_Tracker_00016171/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace Fitness_Tracker_00016171.Models
+{
+    public class User
+    {
+        public int Id { get; set; } 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; } 
+        public List<Activity> Activities { get; set; } 
+    }
+}
